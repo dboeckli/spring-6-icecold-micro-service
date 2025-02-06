f5d5951a70474e0c1539a94b3353611083380ef6
adm-boeckli <dboeckli@gmail.com>
2025-02-06 16:27:01 +0100

    refactor project

--- src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/Spring6IcecoldMicroServiceApplicationTests.java
+++ src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/Spring6IcecoldMicroServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package ch.dboeckli.springframeworkguru.spring6icecoldmicroservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@DirtiesContext
+@Slf4j
 class Spring6IcecoldMicroServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Application...");
     }
 
 }
