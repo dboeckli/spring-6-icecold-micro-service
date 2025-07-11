9af47b4c2038b21ff6276a22a0c11e34da6d422e
Dominique Boeckli <dboeckli@gmail.com>
2025-07-11 11:09:44 +0200

    improve health check (#28)
    * improve health check

--- src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/config/KafkaConfigIT.java
+++ src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/config/KafkaConfigIT.java
@@ -1,9 +1,7 @@
 package ch.dboeckli.springframeworkguru.spring6icecoldmicroservice.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.clients.admin.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +11,9 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+import static ch.dboeckli.springframeworkguru.spring6icecoldmicroservice.listener.DrinkRequestListener.GROUP_ID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @Slf4j
@@ -24,16 +24,25 @@ class KafkaConfigIT {
     private KafkaAdmin kafkaAdmin;
 
     @Test
-    void shouldHaveRequiredTopics() throws Exception {
+    void shouldHaveRequiredTopics() {
         try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
-            ListTopicsResult topicsResult = adminClient.listTopics();
-            Collection<TopicListing> topics = topicsResult.listings().get(5, TimeUnit.SECONDS);
+            await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+                ListTopicsResult topicsResult = adminClient.listTopics();
+                Collection<TopicListing> topics = topicsResult.listings().get(5, TimeUnit.SECONDS);
 
-            assertThat(topics)
-                .extracting(TopicListing::name)
-                .contains(
-                    KafkaConfig.DRINK_REQUEST_ICE_COLD_TOPIC
-                );
+                assertThat(topics)
+                    .extracting(TopicListing::name)
+                    .contains(KafkaConfig.DRINK_REQUEST_ICE_COLD_TOPIC);
+            });
+
+            await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+                ListConsumerGroupsResult consumerGroupsResult = adminClient.listConsumerGroups();
+                Collection<ConsumerGroupListing> groups = consumerGroupsResult.all().get(5, TimeUnit.SECONDS);
+
+                assertThat(groups)
+                    .extracting(ConsumerGroupListing::groupId)
+                    .contains(GROUP_ID);
+            });
         }
     }
 
