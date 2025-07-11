57b00229dc5a0c7d835514def7c8abc9f26e5468
Dominique Boeckli <dboeckli@gmail.com>
2025-07-11 11:02:13 +0200

    improve health check 2

--- src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/config/KafkaConfigIT.java
+++ src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/config/KafkaConfigIT.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 import static ch.dboeckli.springframeworkguru.spring6icecoldmicroservice.listener.DrinkRequestListener.GROUP_ID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @Slf4j
@@ -23,25 +24,25 @@ class KafkaConfigIT {
     private KafkaAdmin kafkaAdmin;
 
     @Test
-    void shouldHaveRequiredTopics() throws Exception {
+    void shouldHaveRequiredTopics() {
         try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
-            ListTopicsResult topicsResult = adminClient.listTopics();
-            Collection<TopicListing> topics = topicsResult.listings().get(5, TimeUnit.SECONDS);
+            await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+                ListTopicsResult topicsResult = adminClient.listTopics();
+                Collection<TopicListing> topics = topicsResult.listings().get(5, TimeUnit.SECONDS);
 
-            ListConsumerGroupsResult consumerGroupsResult = adminClient.listConsumerGroups();
-            Collection<ConsumerGroupListing> groups = consumerGroupsResult.all().get(5, TimeUnit.SECONDS);
+                assertThat(topics)
+                    .extracting(TopicListing::name)
+                    .contains(KafkaConfig.DRINK_REQUEST_ICE_COLD_TOPIC);
+            });
 
-            assertThat(topics)
-                .extracting(TopicListing::name)
-                .contains(
-                    KafkaConfig.DRINK_REQUEST_ICE_COLD_TOPIC
-                );
+            await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+                ListConsumerGroupsResult consumerGroupsResult = adminClient.listConsumerGroups();
+                Collection<ConsumerGroupListing> groups = consumerGroupsResult.all().get(5, TimeUnit.SECONDS);
 
-            assertThat(groups)
-                .extracting(ConsumerGroupListing::groupId)
-                .contains(
-                    GROUP_ID
-                );
+                assertThat(groups)
+                    .extracting(ConsumerGroupListing::groupId)
+                    .contains(GROUP_ID);
+            });
         }
     }
 
